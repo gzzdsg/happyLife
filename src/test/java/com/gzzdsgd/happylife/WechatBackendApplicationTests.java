1290cf99cf63602f898444448eb0318382bbcfad
大美 <damei@damei.local>
2023-12-18 00:50:46 +0800

    init project

---
+++ src/test/java/com/gzzdsgd/happylife/WechatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzzdsgd.happylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
