9aa03a563ac000c6185b5be1a342334a786260f5
gzzdsg <374808065@qq.com>
2024-01-06 20:01:50 +0800

    增加代码注释

--- src/main/java/com/gzzdsgd/happylife/controller/WxController.java
+++ src/main/java/com/gzzdsgd/happylife/controller/WxController.java
@@ -1,7 +1,6 @@
 package com.gzzdsgd.happylife.controller;
 
 
-import com.alibaba.fastjson.JSON;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.gzzdsgd.happylife.constant.Constants;
@@ -14,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * 微信官方对接接口
+ *
+ * @author damei
+ */
 @Slf4j
 @RestController
 public class WxController {
@@ -28,17 +32,21 @@ public class WxController {
      * @return 返回值
      */
     @GetMapping("/wx")
-    public String hello(@RequestParam(value = "signature") String signature,
-                        @RequestParam(value = "timestamp") String timestamp,
-                        @RequestParam(value = "nonce") String nonce,
-                        @RequestParam(value = "echostr") String echostr) {
+    public String wx(@RequestParam(value = "signature") String signature,
+                     @RequestParam(value = "timestamp") String timestamp,
+                     @RequestParam(value = "nonce") String nonce,
+                     @RequestParam(value = "echostr") String echostr) {
 
+        // 将参与验签的参数按照自然序排序
         List<String> params = Lists.newArrayList(Constants.APP_TOKEN, timestamp, nonce);
         List<String> sortedParams = params.stream().sorted().toList();
+        // 三个参数拼接后进行sha1加密
         String encodingStr = EncodeUtils.encodeSha1(Joiner.on("").join(sortedParams));
+        // 成功匹配到则将echostr返回
         if (Objects.equals(encodingStr, signature)) {
             return echostr;
         }
+        log.error("WxController wx -> ");
         return "error";
     }
 }
